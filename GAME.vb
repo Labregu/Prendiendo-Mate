ee61bfe10cc3fc5fbfe1da0bef7a6ca8f0ba3507
luis abregu <40888562@continental.edu.pe>
2019-06-19 06:22:21 -0500

    version1

---
+++ GAME.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+
+End Class
+
+Dim contador As Integer
+
+Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+    Randomize()
+    TextBox1.Text = Int(Rnd() * 10)
+    If Val(TextBox1.Text) = Val(TextBox2.Text) Then
+        MsgBox("You Win")
+        Timer1.Enabled = False
+    Else
+        contador = contador + 1
+        TextBox3.Text = contador
+        TextBox2.Text = ""
+        TextBox2.Focus()
+    End If
+End Sub
+
+Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Timer1.Enabled = True
+End Sub
